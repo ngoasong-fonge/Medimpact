4042684931cdbff96282370ba6d604fbc8454862
ngoasong-fonge <104869381+ngoasong-fonge@users.noreply.github.com>
2023-10-27 10:14:02 -0400

    Update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -58,3 +58,54 @@ maintenance_start
 echo "Maintenance is now complete."| tee -a $LOGDIR/$LOGFILE
 echo "Please re-enable megamaid." | tee -a $LOGDIR/$LOGFILE
 exit 0
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#!/bin/bash
+#
+# This script performs a VACUUM ANALYZE on multiple tables
+#
+
+# Set variables
+echo "Did you shut off mega-maid and the backup?"
+echo "Please input the PostgreSQL password: "
+read -s PGPASSWORD
+
+# Noninteractive variables
+WORKDIR=$(pwd)
+SCRIPT2=maintenance_calls_vacuum_analyze.sh
+DATABASE=mydb
+LOGDIR=$(pwd)
+LOGFILE="maintenance.log"
+i=0
+
+# General log information
+echo "Maintenance (VACUUM ANALYZE) is now beginning" | tee -a $LOGDIR/$LOGFILE
+
+# Function to start maintenance for multiple tables
+vacuum_analyze_start() {
+    # Call children
+    for TABLE in ${TABLES[@]}; do
+        PGPASSWORD=$PGPASSWORD $WORKDIR/$SCRIPT2 $DATABASE $TABLE &
+        PIDS[${i}]=$!
+        PROCS[${i}]=$TABLE
+        i=$(expr $i + 1)
+    done
+
+    # Child Monitor
+    for PID in ${PIDS[*]}; do
+        wait $PID
+    done
+}
+
+# Define an array of tables to perform VACUUM ANALYZE on
+PIDS=()
+TABLES=(table1 table2 table3)
+
+# Start the VACUUM ANALYZE maintenance tasks
+vacuum_analyze_start
+
+echo "Maintenance (VACUUM ANALYZE) is now complete." | tee -a $LOGDIR/$LOGFILE
+echo "Please re-enable mega-maid." | tee -a $LOGDIR/$LOGFILE
+exit 0
