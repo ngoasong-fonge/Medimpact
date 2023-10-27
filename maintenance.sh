cf2aea207af6c84b0643b88ef5d1f38aaf9dbf3a
ngoasong-fonge <104869381+ngoasong-fonge@users.noreply.github.com>
2023-10-27 19:15:47 -0400

    Update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -109,3 +109,25 @@ vacuum_analyze_start
 echo "Maintenance (VACUUM ANALYZE) is now complete." | tee -a $LOGDIR/$LOGFILE
 echo "Please re-enable mega-maid." | tee -a $LOGDIR/$LOGFILE
 exit 0
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#!/bin/bash
+username="your_db_username"
+database="your_db_name"
+tables=("table1" "table2" "table3")
+
+start_time=$(date +%s)
+
+for table in "${tables[@]}"; do
+   psql -U $username -d $database -c "VACUUM ANALYZE $table"
+done
+
+end_time=$(date +%s)
+total_time=$((end_time - start_time))
+
+# Log the total start and end times to a file
+log_file="/path/to/output_directory/vacuum_total_time.log"
+echo "Start Time: $(date -d @$start_time)" > $log_file
+echo "End Time: $(date -d @$end_time)" >> $log_file
+echo "Total Time: $total_time seconds" >> $log_file
